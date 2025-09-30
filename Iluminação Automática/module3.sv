ec8b973cb0cf9c1b15ac464169e4963e60719d31
Josue <geenujunior@gmail.com>
2025-09-30 07:55:51 -0300

    Protótipo módulo 3

---
+++ Iluminação Automática/module3.sv
@@ -0,0 +1,83 @@
+module testeIluminacao #(
+    parameter AUTO_SHUTDOWN_T = 30000
+)(
+    input logic clk,
+    input logic rst, 
+    input logic enable, 
+    input logic infra,
+    output logic C
+);
+
+    parameter qtd_estados = 3;
+    enum logic [qtd_estados-1:0] {INICIAL, CONTANDO, TEMP} estado;
+    logic [15:0] Tc;
+
+
+    // Precisa colocar as autotransições?
+    // Porque no primeiro exemplo ele coloca a autotransição e no segundo não?
+
+    always_ff @(posedge clk or posedge rst) begin
+
+        if (rst == 1) begin
+            estado <= INICIAL;
+            Tc <= 0;
+        end 
+
+        else begin 
+
+            case (estado)
+                INICIAL: begin
+                    Tc <= 0;
+                    if (!infra && enable) begin
+                        estado <= CONTANDO;
+                    end
+                    else begin 
+                         estado <= INICIAL;
+                    end
+                end
+
+                CONTANDO: begin
+                    if (infra) begin
+                        estado <= INICIAL;
+                        // Precisa fazer Tc = 0 ? nmo INICIAL eu faço
+                        Tc <= 0;
+                    end
+                    else if (Tc >= AUTO_SHUTDOWN_T) begin
+                        estado <= TEMP;
+                        Tc <= 0;
+                    end
+                    else begin
+                        estado <= CONTANDO;
+                        Tc <= Tc + 1;
+                    end
+                end
+
+                TEMP: begin
+                    estado <= INICIAL;
+                end
+
+                default: begin 
+                    estado <= INICIAL;
+                    // Preciso zerar o contador?
+                    Tc <= 0;
+                end
+            endcase
+
+        end    
+
+    end
+
+    always_comb begin
+
+        case(estado)
+
+            INICIAL  : C = 0;
+            CONTANDO : C = 0;
+            TEMP     : C = 1;
+            default  : C = 0;
+
+        endcase
+        
+    end
+
+endmodule
