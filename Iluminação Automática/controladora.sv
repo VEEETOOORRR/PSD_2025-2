70651333dc848e5f06f44c6b0bfde50511b3ba1d
VEEETOOORRR <antonni2601.ag@gmail.com>
2025-10-01 22:02:55 -0300

    correções de sintaxe

--- Iluminação Automática/controladora.sv
+++ Iluminação Automática/controladora.sv
@@ -9,6 +9,7 @@ output 	    logic	led,
 output		logic	saida );
 
 logic clk_1khz;
+logic A, B, C, D;
 
 divfreq df(
     .clk(clk),
@@ -16,16 +17,34 @@ divfreq df(
     .clk_i(clk_1khz)
 );
 
+submodulo_1 sm1 (
+    .clk(clk_1khz),
+    .rst(rst),
+    .push_button(push_button),
+    .a(A),
+    .b(B),
+    .c(C),
+    .d(infravermelho)
+);
+
+
 submodulo_2 #(.DEBOUNCE_P(DEBOUNCE_P), .SWITCH_MODE_MIN_T(SWITCH_MODE_MIN_T)) sm2 (
     .clk(clk_1khz),
     .rst(rst),
     .push_button(push_button),
-    .A(),
-    .B()
+    .A(A),
+    .B(B)
+);
+
+submodulo_3 #(.AUTO_SHUTDOWN_T(AUTO_SHUTDOWN_T)) sm3 (
+    .clk(clk_1khz),
+    .rst(rst),
+    .push_button(push_button),
+    .C(C)
 );
 
 
 
 
 
-endmodule;
+endmodule
